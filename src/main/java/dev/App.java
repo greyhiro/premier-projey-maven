4566ae00873968f9fb1292e1c41ea425878975f8
Pauline SOCA <pauline.soca@gmail.com>
2018-01-05 14:55:59 +0100

    tp 2 reussi

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,5 @@
 package dev;
-
+import com.github.lalyos.jfiglet.FigletFont;
 /**
  * Hello world!
  *
@@ -8,7 +8,13 @@ public class App
 {
     public static void main( String[] args )
     {
+    	   String asciiArt = FigletFont.convertOneLine("SOQUETTE");
+           System.out.println(asciiArt);
         System.out.println( "Hello World!" );
        
     }
+    
+
+
+
 }
